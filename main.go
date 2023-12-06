2ccace1cff9dd78278946f7e42bd5da4a4892a21
kraii <matthewkerry@gmail.com>
2023-12-06 19:13:52 +0000

    initial sort

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type RatedPlayer struct {
+	player *Player
+	rating float64
+}
+
+type ByRating []*RatedPlayer
+
+func (a ByRating) Len() int           { return len(a) }
+func (a ByRating) Less(i, j int) bool { return a[i].rating < a[j].rating }
+func (a ByRating) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 func main() {
-	players, err := Parse("/media/kraii/Windows/Users/matth/Documents/scout/team.html")
+	players, err := Parse("/media/kraii/Windows/Users/matth/Documents/scout/dm.html")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, p := range players {
-		fmt.Printf("%s rated %.3f\n", p.name, Rate(p, &DefensiveMid))
+	ratedPlayers := make([]*RatedPlayer, len(players))
+	for i, p := range players {
+		ratedPlayers[i] = &RatedPlayer{
+			p,
+			Rate(p, &DefensiveMid),
+		}
+	}
+	sort.Sort(ByRating(ratedPlayers))
+
+	for _, rp := range ratedPlayers {
+		fmt.Printf("%s rated %.3f\n", rp.player.name, rp.rating)
 	}
 }
